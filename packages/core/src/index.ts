2094dd087b322aff6795252416caa495a5f67af0
Ibe Dwi <dwisatriakusuma4@gmail.com>
2023-08-05 22:01:10 +0700

    feat: add core with test

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,28 @@
-function core() {
-  console.log("this is from core");
+import { z } from "zod";
+
+export const schema = z.object({
+  collectionName: z.optional(z.string()),
+  variables: z.array(
+    z.object({
+      name: z.string(),
+      value: z.string(),
+      description: z.optional(z.string()),
+      type: z.string(),
+    })
+  ),
+});
+
+type Config = z.infer<typeof schema>;
+
+function getFormattedConfig(configString: string): Promise<Config> {
+  return new Promise((resolve, reject) => {
+    try {
+      const parsed = schema.parse(JSON.parse(configString));
+      resolve(parsed);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
-export { core };
+export { getFormattedConfig };
