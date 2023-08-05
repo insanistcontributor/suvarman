81f477a18beed292e4bec24da5dcf7908d114407
Ibe Dwi <dwisatriakusuma4@gmail.com>
2023-08-06 00:24:53 +0700

    feat: add simple logo

--- apps/figma-variables-manager-plugin/src/app/app.tsx
+++ apps/figma-variables-manager-plugin/src/app/app.tsx
@@ -5,6 +5,7 @@ import { getFormattedConfig } from "@fvm/core";
 import { FigmaMessage } from "../messages";
 import { processConfigFSM } from "./processConfig.fsm";
 import { useMachine } from "@xstate/react";
+import { HiOutlineCog } from "react-icons/hi";
 
 function App() {
   const [machineState, machineSend] = useMachine(processConfigFSM, {
@@ -108,44 +109,22 @@ function App() {
 
   return (
     <div className="p-4">
-      <h1 className="heading">Convert JSON to Figma Variable</h1>
-      {/* <input
-        id="file-selector"
-        type="file"
-        multiple={false}
-        accept=".json"
-        onChange={(e) => onReadFile(e)}
-        onClick={() => console.log("asdf")}
-      /> */}
+      <div className="w-full flex flex-col items-center mb-5">
+        <HiOutlineCog size={50} className="animate-spin" />
+        <h1 className="text-xl">Variable Manager</h1>
+        <h1 className="text-md text-gray-400">
+          Convert JSON to Figma Variable
+        </h1>
+      </div>
 
       <div className="col-span-full">
-        <label
-          htmlFor="config-name"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          Config JSON
-        </label>
+        <p className="block text-sm font-medium leading-6 text-gray-900">
+          Config file
+        </p>
 
         <div className="mt-2">
           <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            {/* <input
-              type="text"
-              name="config-name"
-              id="config-name"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="Select file"
-              value={file?.name}
-              disabled
-            /> */}
-            <div
-              // type="text"
-              // name="config-name"
-              // id="config-name"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              // placeholder="Select file"
-
-              // disabled
-            >
+            <div className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
               {file?.name ?? "Select file"}
             </div>
           </div>
@@ -156,9 +135,6 @@ function App() {
             className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 text-sm"
           >
             <span>Select config file (.json)</span>
-            {/* <span className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              <span>Select config file (.json)</span>
-            </span> */}
             <input
               type="file"
               className="sr-only"
@@ -166,13 +142,12 @@ function App() {
               multiple={false}
               accept=".json"
               onChange={(e) => onReadFile(e)}
-              onClick={() => console.log("asdf")}
             />
           </label>
         </div>
       </div>
 
-      <div className="mt-6 w-full ">
+      <div className="mt-6 w-full flex justify-end">
         <button
           onClick={onCreateVariables}
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
