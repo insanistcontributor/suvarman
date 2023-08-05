3f38e014cd1c89f093f8cf52209279782ecbed8f
Ibe Dwi <hi@ibedwi.dev>
2023-08-06 00:38:29 +0700

    [plugin] Update UI  (#3)
    * feat: update UI
    
    * feat: add simple logo

--- apps/figma-variables-manager-plugin/src/app/app.tsx
+++ apps/figma-variables-manager-plugin/src/app/app.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { getFormattedConfig } from "@fvm/core";
 import { FigmaMessage } from "../messages";
 import { processConfigFSM } from "./processConfig.fsm";
 import { useMachine } from "@xstate/react";
+import { HiOutlineCog } from "react-icons/hi";
 
 function App() {
   const [machineState, machineSend] = useMachine(processConfigFSM, {
@@ -40,9 +41,12 @@ function App() {
     },
   });
 
+  const [file, setFile] = useState<File | null>();
+
   const onReadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = document.getElementById("file-selector") as HTMLInputElement;
     if (input.files && input.files[0]) {
+      setFile(input.files[0]);
       const reader = new FileReader();
       reader.onload = (e) => {
         const text = e.target?.result as string;
@@ -104,18 +108,53 @@ function App() {
   }, [machineState.value]);
 
   return (
-    <div className="bg-red-100">
-      <h1 className="heading">Convert JSON to Figma Variable</h1>
-      <button onClick={onCreateVariables}>Create collection</button>
+    <div className="p-4">
+      <div className="w-full flex flex-col items-center mb-5">
+        <HiOutlineCog size={50} className="animate-spin" />
+        <h1 className="text-xl">Variable Manager</h1>
+        <h1 className="text-md text-gray-400">
+          Convert JSON to Figma Variable
+        </h1>
+      </div>
 
-      <input
-        id="file-selector"
-        type="file"
-        multiple={false}
-        accept=".json"
-        onChange={(e) => onReadFile(e)}
-        onClick={() => console.log("asdf")}
-      />
+      <div className="col-span-full">
+        <p className="block text-sm font-medium leading-6 text-gray-900">
+          Config file
+        </p>
+
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <div className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+              {file?.name ?? "Select file"}
+            </div>
+          </div>
+        </div>
+        <div className="my-2 w-full justify-end flex">
+          <label
+            htmlFor="file-selector"
+            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 text-sm"
+          >
+            <span>Select config file (.json)</span>
+            <input
+              type="file"
+              className="sr-only"
+              id="file-selector"
+              multiple={false}
+              accept=".json"
+              onChange={(e) => onReadFile(e)}
+            />
+          </label>
+        </div>
+      </div>
+
+      <div className="mt-6 w-full flex justify-end">
+        <button
+          onClick={onCreateVariables}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Create Variable
+        </button>
+      </div>
     </div>
   );
 }
